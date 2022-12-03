113fbf88865b3dcfba1f68b3c2ee537120c306f2
dkStephanos <29663585+dkStephanos@users.noreply.github.com>
2022-12-03 17:34:40 -0500

    adding cleanse process for innings results

--- jupyter/code/preprocessing.py
+++ jupyter/code/preprocessing.py
@@ -2,10 +2,10 @@ def cleanse_match_results(
     match_results_df,
     cols_to_keep=['matchid', 'dates', 'gender', 'outcome.winner', 'teams'],
 ):
-    # *********************************************************************************************************
-    # Accepts match results and innings results, drops uneeded columns, imputes incomplete information,
-    # and filters matches to only those with natrual winner (exludings DLS decisions, ties, etc.)
-    # *********************************************************************************************************
+    # ************************************************************************************
+    # Accepts match results, drops uneeded columns and filters matches to only those
+    # with natrual winner (exludings DLS decisions, ties, etc.)
+    # ************************************************************************************
 
     # Removes all games decided by DLS
     match_results_df = match_results_df.loc[match_results_df["outcome.method"].isna()]
@@ -28,3 +28,29 @@ def split_by_gender(df):
     return df.loc[df['gender'] == 'male'].drop(['gender'], axis=1), df.loc[
         df['gender'] == 'female'
     ].drop(['gender'], axis=1)
+
+
+def cleanse_innings_results(innings_results_df, match_results_df):
+    # ************************************************************************************
+    # Accepts innings results and match_results_df and filters/returns innings results
+    # for only those matches that end in a result
+    # ************************************************************************************
+
+    matchids = _collect_matchids(match_results_df)
+    innings_results_df = innings_results_df.loc[innings_results_df['matchid'].isin(matchids)]
+
+    return innings_results_df
+
+
+
+
+def _collect_matchids(match_results_df):
+    # *******************************************************************************
+    # Filters match_results to those that have a result, and returns those matchids
+    # *******************************************************************************
+    match_results_df['result'].unique()
+    return match_results_df.loc[
+        (match_results_df["result"].isna()) | (match_results_df["result"] == "tied")
+    ]['matchid'].unique()
+
+

5710552481d55aa7772916258335f2bb5ef2f65c
dkStephanos <29663585+dkStephanos@users.noreply.github.com>
2022-12-04 21:12:46 -0500

    refactore, pulling out helper method

--- jupyter/code/modeling.py
+++ jupyter/code/modeling.py
@@ -1,27 +1,19 @@
+import pickle
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
-import pandas as pd
-import pickle
+
 
 def predict_runs_per_over(
     overs_df,
     feature_cols=["Innings", "RemainingOvers", "RemainingWickets"],
     test_size=0.3,
 ):
-
-    # Select the target_col
-    y = overs_df["RunsPerOver"]
-
-    # Select the feature cols, and normalize them to avoid scale bias
-    X = StandardScaler().fit_transform(overs_df[feature_cols])
-
     # Split the data into training/testing sets
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=test_size, random_state=42
+    X_train, X_test, y_train, y_test = _prepare_train_test_data(
+        overs_df, feature_cols, test_size
     )
-
     # Fit the model, generate predictions on test set
     regr = LinearRegression().fit(X_train, y_train)
     y_pred = regr.predict(X_test)
@@ -35,6 +27,7 @@ def predict_runs_per_over(
 
     pickle.dump(regr, open('./data/models/linear-regression-runsPerOver.sav', 'wb'))
 
+
 def apply_model(X_test, y_test):
     loaded_model = pickle.load(
         open('./data/models/linear-regression-runsPerOver.sav', 'rb')
@@ -42,3 +35,13 @@ def apply_model(X_test, y_test):
     result = loaded_model.score(X_test, y_test)
     print(result)
 
+
+def _prepare_train_test_data(df, feature_cols, test_size):
+    # Select the target_col
+    y = df["RunsPerOver"]
+
+    # Select the feature cols, and normalize them to avoid scale bias
+    X = StandardScaler().fit_transform(df[feature_cols])
+
+    # Split the data into training/testing sets
+    return train_test_split(X, y, test_size=test_size, random_state=42)
